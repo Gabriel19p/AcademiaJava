db74f0e5b37311fd1ce1595de294e589946fd20e
Gabriel Freitas <122943129+Gabriel19p@users.noreply.github.com>
2023-04-26 19:11:53 -0300

    Update BancoDados.java

--- ExercicioJDBC/src/jdbc/BancoDados.java
+++ ExercicioJDBC/src/jdbc/BancoDados.java
@@ -3,30 +3,34 @@ package jdbc;
 import java.sql.*;
 
 public class BancoDados implements InterfaceBancoDados {
+	
+	
+	private Connection conn;
+	@Override
+	public void conectar(String db_url, String db_user, String db_password) {
+		  
+		try {
+	            conn = DriverManager.getConnection(db_url, db_user, db_password);
+	            System.out.println("Banco de dados Conectado.");
+	        } catch (SQLException e) {
+	            System.out.println("Erro ao conectar ao banco de dados: " + e.getMessage());
+	        }
+		
+	}
 
+	@Override
+	public void desconectar() {
+	       try {
+	            conn.close();
+	            System.out.println("Conexão com banco de dados encerrada.");
+	        } catch (SQLException e) {
+	            System.out.println("Erro ao desconectar do banco de dados: " + e.getMessage());
+	        }
+	}
 
-    private Connection conn;
-
-    public void conectar(String db_url, String db_user, String db_password) {
-        try {
-            conn = DriverManager.getConnection(db_url, db_user, db_password);
-            System.out.println("Banco de dados Conectado.");
-        } catch (SQLException e) {
-            System.out.println("Erro ao conectar ao banco de dados: " + e.getMessage());
-        }
-    }
-
-    public void desconectar() {
-        try {
-            conn.close();
-            System.out.println("Conexão com banco de dados encerrada.");
-        } catch (SQLException e) {
-            System.out.println("Erro ao desconectar do banco de dados: " + e.getMessage());
-        }
-    }
-
-    public void consultar(String db_query) {
-        try {
+	@Override
+	public void consultar(String db_query) {
+		try {
             Statement stmt = conn.createStatement();
             ResultSet rs = stmt.executeQuery(db_query);
 
@@ -40,20 +44,23 @@ public class BancoDados implements InterfaceBancoDados {
         } catch (SQLException e) {
             System.out.println("Erro ao executar consulta: " + e.getMessage());
         }
-    }
+		
+	}
 
-    public int inserirAlterarExcluir(String db_query) {
-        try {
-            Statement stmt = conn.createStatement();
-            int result = stmt.executeUpdate(db_query);
-            int linhas = result;
-			System.out.println("A operacao afetou: "+linhas+" linhas.");
-            stmt.close();
-            return result;
-        } catch (SQLException e) {
-            System.out.println("Erro ao executar inserção/alteração/exclusão: " + e.getMessage());
-            return -1;
-        }
-    }
+	@Override
+	public int inserirAlterarExcluir(String db_query) {
+		 try {
+	            Statement stmt = conn.createStatement();
+	            int result = stmt.executeUpdate(db_query);
+	            int linhas = result;
+				System.out.println("A operacao afetou: "+linhas+" linhas.");
+	            stmt.close();
+	            return result;
+	        } catch (SQLException e) {
+	            System.out.println("Erro ao executar inserção/alteração/exclusão: " + e.getMessage());
+	            return -1;
+	        }
+	}
+
+   
 }
-	
